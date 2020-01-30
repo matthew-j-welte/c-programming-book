96df1c08f5c38999d9752a9ffac78eb4be4ac95f
Matthew Welte <matthew.welte95@gmail.com>
2020-01-29 23:48:59 -0500

    escape and unescape functions

---
+++ chapter3/escape.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+void escape(char s[], char t[]);
+void unescape(char s[], char t[]);
+
+int main() {
+    char a[] = "Hello\nThere\t\tFella";
+    char b[100];
+    escape(b, a);
+    printf("%s\n", b);
+    char c[] = "Hello\\nThere\\t\\tDog";
+    char d[100];
+    unescape(d,c);
+    printf("%s\n", d);
+    return 0;
+}
+
+void escape(char s[], char t[]) {
+    int c,i,j;
+    for(i = j = 0; (c = t[i]) != '\0'; ++i) {
+        switch (c) {
+        case '\n': case '\t': case '\b':
+            s[j++] = '\\';
+            switch (c) {
+            case '\n':
+                c = 'n';
+                break;
+            case '\t':
+                c = 't';
+                break;
+            case '\b':
+                c = 'b';
+                break;
+            }
+            break;
+        default:
+            break;
+        }
+        s[j++] = c;
+    }
+}
+
+void unescape(char s[], char t[]) {
+    int c,i,j;
+    for(i = j = 0; (c = t[i]) != '\0'; ++i) {
+        switch (c) {
+        case '\\':
+            c = t[i + 1];
+            switch (c) {
+            case 't':
+                c = '\t';
+                ++i;
+                break;
+            case 'n':
+                c = '\n';
+                ++i;
+                break;
+            case 'b':
+                c = '\b';
+                ++i;
+                break;
+            default:
+                c = t[i];
+                break;
+            }
+        default:
+            break;
+        }
+        s[j++] = c;
+    }
+}
